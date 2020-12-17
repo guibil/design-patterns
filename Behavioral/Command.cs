e2cbd2d9435b83ad5e92a159bad77ad6757c47c4
Sagynbek <kenzhebaevsagynbek@gmail.com>
2020-12-17 21:20:49 +0300

    Behavioral Command pattern new example added

--- Behavioral/Command.cs
+++ Behavioral/Command.cs
@@ -2,7 +2,9 @@
 
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
+using Console = System.Console;
 
 #endregion
 
@@ -115,10 +117,10 @@ namespace DesignPatterns.Behavioral
         public void Execute()
         {
             var ba = new BankAccount();
-            var commands = new List<BankAccountCommand>
+            var commands = new List<ICommand>
             {
                 new BankAccountCommand(ba, BankAccountCommand.Action.Deposit, 100),
-                new BankAccountCommand(ba, BankAccountCommand.Action.Withdraw, 1000)
+                new BankAccountCommand(ba, BankAccountCommand.Action.Withdraw, 50)
             };
 
             Console.WriteLine(ba);
@@ -132,6 +134,80 @@ namespace DesignPatterns.Behavioral
                 c.Undo();
 
             Console.WriteLine(ba);
+        }
+    }
+
+
+    public interface IControlCommand
+    {
+        void Execute();
+    }
+    public abstract class Control : IControlCommand
+    {
+        public Point Location { get; set; }
+        public int Width { get; set; }
+        public string Text { get; set; }
+
+        public abstract void Execute();
+    }
+    public class ExitButton : Control
+    {
+        public override void Execute()
+        {
+            Console.WriteLine("Exit Environment...");
+        }
+    }
+    public class MaxButton : Control
+    {
+        public override void Execute()
+        {
+            Console.WriteLine("Maximizing window...");
+        }
+    }
+
+    public abstract class Control2 : IControlCommand
+    {
+        public abstract void Execute();
+    }
+    public class Timer :Control2
+    {
+        public override void Execute()
+        {
+            Console.WriteLine("Time ticking...");
+        }
+    }
+
+    public class Window
+    {
+        public IControlCommand CloseButton;
+        public IControlCommand MaximizeButton;
+        public IControlCommand timer;
+
+        public Window()
+        {
+            CloseButton = new ExitButton()
+            {
+                Text = "Çıkış",
+                Location = new Point(200, 200),
+                Width = 100
+            };
+
+            MaximizeButton = new MaxButton()
+            {
+                Text = "Max",
+                Location = new Point(1200, 10),
+                Width = 50
+            };
+            timer = new Timer();
+        }
+    }
+    public class WindowRunner
+    {
+        public void Execute()
+        {
+            var window = new Window();
+            window.MaximizeButton.Execute();
+            window.CloseButton.Execute();
         }
     }
 }
